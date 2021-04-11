194f18075d5a9c2845a0aa735903f6f9614d33b0
Ragab Muhamed Ragab <mohamaragab0@gmail.com>
2021-04-11 18:09:17 +0200

    String.Copy(Str)

---
+++ Copy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Ragab";
+           string str1= CopyStr(str);
+            Console.WriteLine(str1);
+          
+        }
+       public static string CopyStr(string str)
+        {
+            return str;
+        }
+
+     
+    }
+}
+
