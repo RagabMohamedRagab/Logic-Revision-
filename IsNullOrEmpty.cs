f2413c5abe8ff149a02e2d384d63018cb6552d8d
Ragab Muhamed Ragab <mohamaragab0@gmail.com>
2021-04-11 17:56:02 +0200

    IsNullOrEmpty()

---
+++ IsNullOrEmpty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = String.Empty;
+            Console.WriteLine(IsNull(str));
+          
+        }
+        public static bool IsNull(string str)
+        {
+            return (str == "" || str == null ? true : false);
+        }
+
+     
+    }
+}
+
